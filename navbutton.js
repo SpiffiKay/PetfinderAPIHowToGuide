3c556043f2447d64af1040fb63a2b809d5753712
frieseti <frieseti@oregonstate.edu>
2017-03-01 13:09:16 -0500

    First

---
+++ navbutton.js
@@ -0,0 +1,20 @@
+var button = document.getElementById("navButton");
+var basic = document.getElementById("basic");
+var setup = document.getElementById("setup");
+var use1 = document.getElementById("use1");
+var use2 = document.getElementById("use2");
+
+button.addEventListener("click", function(event){
+  basic.addEventListener("click", function(event){
+    console.log("basic!");
+  });
+  setup.addEventListener("click", function(event){
+    console.log("setup!");
+  });
+  use1.addEventListener("click", function(event){
+    console.log("use1!");
+  });
+  use2.addEventListener("click", function(event){
+    console.log("use2!");
+  });
+});
