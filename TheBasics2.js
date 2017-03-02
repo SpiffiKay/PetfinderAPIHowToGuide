94695d561d6ff25917f55b1943ee3ab2ca68f104
frieseti <frieseti@oregonstate.edu>
2017-03-02 14:33:30 -0500

    third

--- TheBasics2.js
+++ TheBasics2.js
@@ -1,5 +1,5 @@
 var nextPage = document.getElementById("nextPage");
 
 nextPage.addEventListener("click", function(event){
-    window.location.href = "https://spiffikay.github.io/TheBasics2.html"
+    window.location.href = "https://spiffikay.github.io/SetItUp.html"
   });
--- navbutton.js
+++ navbutton.js
@@ -1,5 +1,6 @@
 var button = document.getElementById("navButton");
 var basic = document.getElementById("basic");
+var basic2 = document.getElementById("basic2");
 var setup = document.getElementById("setup");
 var use1 = document.getElementById("use1");
 var use2 = document.getElementById("use2");
@@ -7,6 +8,9 @@ var use2 = document.getElementById("use2");
 button.addEventListener("click", function(event){
   basic.addEventListener("click", function(event){
     window.location.href = "https://spiffikay.github.io/TheBasics.html";
+  });
+  basic2.addEventListener("click", function(event){
+    window.location.href = "https://spiffikay.github.io/TheBasics2.html";
   });
   setup.addEventListener("click", function(event){
     window.location.href = "https://spiffikay.github.io/SetItUp.html";
