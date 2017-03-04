6bc0ab8dc528cddaaf51588fbccfd5428ceb0212
frieseti <frieseti@oregonstate.edu>
2017-03-04 17:52:22 -0500

    Add files via upload

--- petAPI.js
+++ petAPI.js
@@ -3,21 +3,21 @@
 
 
 
+
+/*
   $(document).ready(function(){
   var animal = "dog";
   $.getJSON("http://api.petfinder.com/breed.list?format=json&animal=" + animal +  "&key=7f275b00b7e879ab40ec03ca8ab5c95a&callback=?")
     .done(function(petApiData) { console.log(petApiData); })
     .fail(function(err) { alert('Error retrieving data!');})
-  //event.preventDefault();
 });
 /*breed list*/
+*/
 
 
-/*
 $(document).ready(function(){
 $.getJSON('http://api.petfinder.com/shelter.find?format=json&animal=cat&key=7f275b00b7e879ab40ec03ca8ab5c95a&callback=?')
   .done(function(petApiData) { console.log(petApiData); })
   .fail(function(err) { console.log('Error retrieving data!');
 });
 });
-*/
